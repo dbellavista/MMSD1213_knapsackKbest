0170bc393b333a6c0dfe718502768ecaf3f5cc59
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-07-28 09:31:10 +0200

    Debug messages utilities

---
+++ knapsackkbest/include/kp_alg/debug.h
@@ -0,0 +1,29 @@
+/*
+ * debug.h
+ *
+ *  Created on: Jul 28, 2013
+ *      Author: Daniele Bellavista <daniele.bellavista@studio.unibo.it>
+ */
+
+#ifndef DEBUG_H_
+#define DEBUG_H_
+
+#include "kp_alg.h"
+
+enum debug_level {
+	NOTICE = 0x1,
+	WARNING = 0x2,
+	ERROR = 0x4,
+	DEBUG = 0x8
+};
+
+void set_debug_level(uint32 debug);
+void d_notice(char* format, ...);
+void d_warning(char* format, ...);
+void d_error(char* format, ...);
+void d_debug(char* format, ...);
+
+void d_inc_indent();
+void d_dec_indent();
+
+#endif /* DEBUG_H_ */
---
+++ knapsackkbest/src/debug.c
@@ -0,0 +1,77 @@
+/*
+ * debug.c
+ *
+ *  Created on: Jul 28, 2013
+ *      Author: Daniele Bellavista <daniele.bellavista@studio.unibo.it>
+ */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "../include/kp_alg/debug.h"
+
+static int debug_level = NOTICE ^ WARNING ^ ERROR ^ DEBUG;
+static int indent = 0;
+
+void printIndent(FILE* stream) {
+	int i;
+	for(i = 0; i < indent; i++) {
+		fprintf(stream, "\t");
+	}
+}
+
+void set_debug_level(uint32 debug) {
+	debug_level = debug;
+}
+
+void d_notice(char* format, ...) {
+	if (debug_level & NOTICE) {
+		va_list argptr;
+		va_start(argptr, format);
+		printIndent(stdout);
+		fprintf(stdout, "- NOTICE: ");
+		vfprintf(stdout, format, argptr);
+		va_end(argptr);
+	}
+}
+
+void d_warning(char* format, ...) {
+	if (debug_level & WARNING) {
+		va_list argptr;
+		va_start(argptr, format);
+		printIndent(stdout);
+		fprintf(stdout, "+ WARNING: ");
+		vfprintf(stdout, format, argptr);
+		va_end(argptr);
+	}
+}
+
+void d_error(char* format, ...) {
+	if (debug_level & ERROR) {
+		va_list argptr;
+		va_start(argptr, format);
+		printIndent(stdout);
+		fprintf(stdout, "* ERROR: ");
+		vfprintf(stdout, format, argptr);
+		va_end(argptr);
+	}
+}
+
+void d_debug(char* format, ...) {
+	if (debug_level & DEBUG) {
+		va_list argptr;
+		va_start(argptr, format);
+		printIndent(stdout);
+		fprintf(stdout, "^ DEBUG: ");
+		vfprintf(stdout, format, argptr);
+		va_end(argptr);
+	}
+}
+
+void d_inc_indent() {
+	indent++;
+}
+void d_dec_indent() {
+	if (indent) {
+		indent--;
+	}
+}

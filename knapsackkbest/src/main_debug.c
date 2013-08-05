ba7fe87d18b03d7a7665000ab95e56034b6c4613
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-08-05 15:38:00 +0200

    Files for debugging the algorithm

---
+++ knapsackkbest/src/main_debug.c
@@ -0,0 +1,32 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 5, 2013
+ *      Author: Daniele Bellavista <daniele.bellavista@studio.unibo.it>
+ */
+
+#include "../include/io_utility.h"
+#include "../include/debug.h"
+#include "../include/kbest.h"
+
+int main(int argc, char **argv) {
+
+
+	// set_debug_level(WARNING ^ ERROR ^ NOTICE);
+
+	KProblem problem;
+	KBestSolutions solutions;
+	read_problem(&problem, "test.in");
+	print_kproblem(problem);
+
+	kp_solve(&solutions, problem, 15);
+
+	print_kbest_solution(solutions);
+
+
+	kp_free_kp(problem);
+	kp_free_kbest_sols(solutions);
+
+	return 0;
+}
+

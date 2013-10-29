2cded9b63e4f87c0e1e23b8bfa5a43123a6d9836
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-10-29 23:20:06 +0100

    get real time utilities

---
+++ knapsackkbest/src/demo/get_real_time.h
@@ -0,0 +1,19 @@
+/**
+ *
+ * @file    get_real_time.h
+ * @author  Daniele Bellavista (daniele.bellavista@studio.unibo.it)
+ * @date    10/29/2013 11:17:13 PM
+ * @brief   Utility for obtaining the real time
+ *
+ * @details 
+ * Site:    http://NadeauSoftware.com/
+ * License: Creative Commons Attribution 3.0 Unported License
+ *          http://creativecommons.org/licenses/by/3.0/deed.en_US
+ */
+
+#ifndef  __GET_REAL_TIME_INC__
+#define  __GET_REAL_TIME_INC__
+
+double get_real_time();
+
+#endif   /* ----- #ifndef __GET_REAL_TIME_INC__  ----- */

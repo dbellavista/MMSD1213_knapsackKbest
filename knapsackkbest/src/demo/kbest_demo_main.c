f73b041aa41083a519b16c93f1dddf58f4635154
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-10-30 22:29:13 +0100

    Iterative and 'project' mode

--- knapsackkbest/src/demo/kbest_demo_main.c
+++ knapsackkbest/src/demo/kbest_demo_main.c
@@ -13,15 +13,20 @@
 #include "kbest.h"
 #include "get_real_time.h"
 
+#ifndef MAX_PATH
+#define MAX_PATH 4096
+#endif
+
+
 KBestSolutions single_test(char* filename, uint32_t K, double* test_time, bool
     do_prints);
 void print_help();
 void time_test(char* res_file, char** files, size_t files_num, uint32_t
     interaction, uint32_t K);
-void project_test(char* sol_file, char* time_file, char** files, size_t
-    files_num, uint32_t K);
+void project_test(char* base, char* K_list, uint32_t max_n);
 void print_array2(uint32_t* array, size_t size);
 void check_solutions(KBestSolutions solutions);
+void interactive_test();
 
 void print_array2(uint32_t* array, size_t size)
 {
@@ -57,46 +62,195 @@ KBestSolutions single_test(char* filename, uint32_t K, double* test_time, bool d
 	return solutions;
 }
 
-void project_test(char* sol_file, char* time_file, char** files, size_t
-    files_num, uint32_t K)
+void interactive_test()
 {
-	size_t fileid;
+  char filename[MAX_PATH];
+  char klist[MAX_PATH];
+  filename[0] = 0;
+  uint32_t K, maxN;
+  FILE* f;
+  bool ok_filename = false, ok_k = false;
+  double test_time;
+  bool selected_1 = false;
+  KBestSolutions sol;
+
+  printf("\n\n[*] Bellavista Daniele.\n"
+             "[*] KBEST SOLVER , Interactive mode:\n\n");
+
+  printf("[1] Solve a single problem\n");
+  printf("[2] Solve a library type\n");
+  printf("[-] The choice is yours: ");
+  while(1) {
+    fflush(stdout);
+    if (fgets(filename, MAX_PATH - 1, stdin) == NULL) {
+      printf("\n[#] Bye!\n");
+      return;
+    }
+    if(filename[0] == '1') {
+      selected_1 = true;
+      break;
+    } else if(filename[0] == '2') {
+      selected_1 = false;
+      break;
+    }
+  }
+  if(selected_1) {
+    while(!ok_filename) {
+      printf("\t[+] Set the problem to solve: ");
+      fflush(stdout);
+      if (fgets(filename, MAX_PATH - 1, stdin) == NULL) {
+        printf("\n[#] Bye!\n");
+        return;
+      }
+      filename[MAX_PATH - 1] = 0;
+      filename[strlen(filename) - 1] = 0;
+      f = fopen(filename, "r");
+      if(f != NULL) {
+        ok_filename = true;
+      } else {
+        printf("\t[*] Sorry %s doesn't exists...\n", filename);
+      }
+    }
+    while(!ok_k) {
+      printf("\t[+] Set K: ");
+      fflush(stdout);
+      if(scanf(" %d", &K) != 1 || K == 0) {
+        printf("[*] Sorry, K must be a positive integer\n");
+      }
+      ok_k = true;
+    }
+    printf("[+] Solving the solutions in b_solutions_single.out\n");
+    sol = single_test(filename, K, &test_time, false);
+    printf("[+] Solve time: %f s\n", test_time);
+    f = fopen("b_solutions_single.out", "w");
+    print_kbest_solution_default_format(f, sol);
+    fclose(f);
+  } else {
+    printf("\t[+] Set the base directory of the instances (a path /path/nxx/): ");
+    fflush(stdout);
+    if (fgets(filename, MAX_PATH - 1, stdin) == NULL) {
+      printf("\n[#] Bye!\n");
+      return;
+    }
+    filename[MAX_PATH - 1] = 0;
+    filename[strlen(filename) - 1] = 0;
+    printf("\t[+] Set a K list (comma separated): ");
+    fflush(stdout);
+    if (fgets(klist, MAX_PATH - 1, stdin) == NULL) {
+      printf("\n[#] Bye!\n");
+      return;
+    }
+    while(1) {
+      printf("\t[+] Set the maximum n present: ");
+      fflush(stdout);
+      if(scanf(" %d", &maxN) != 1 || maxN == 0) {
+        printf("[*] Sorry, m must be a positive integer\n");
+        continue;
+      }
+      break;
+    }
+    klist[MAX_PATH - 1] = 0;
+    klist[strlen(filename) - 1] = 0;
+    printf("[+] Going to solve... It might take a while\n");
+    project_test(filename, klist, maxN);
+  }
+}
+
+void project_test(char* base, char* K_list, uint32_t max_n)
+{
 	FILE *sol_f, *time_f;
-	double test_time, max_time = 0.0, min_time = INFINITY, average_time = 0.0;
 	KBestSolutions solutions;
+  double test_time, max_time, min_time, average_time;
+	uint32_t files_num, curr_n;
+	char path[MAX_PATH];
+	FILE* f;
+  char *token, *string, *tofree;
+  uint32_t K;
+  bool written_header;
 
-  // Performing tests
-	for (fileid = 0; fileid < files_num; fileid++) {
-	  d_notice("Solving file %s\n", files[fileid]);
-	  solutions = single_test(files[fileid], K, &test_time, false);
-    if(fileid + 1 < files_num)
-      kp_free_kbest_sols(solutions);
-    if(test_time > max_time) {
-      max_time = test_time;
+  d_notice("Starting the project solve on %s\n", base);
+  d_inc_indent();
+  d_notice("Note that this will bruteforce the n*** folders!\n");
+  d_dec_indent();
+
+  for(curr_n = 1; curr_n <= max_n; curr_n++) {
+
+    sprintf(path, "%s/n%u/i0/test.in", base, curr_n);
+    f = fopen(path, "r");
+    if(f == NULL) {
+      continue;
     }
-    if(test_time < min_time) {
-      min_time = test_time;
+    fclose(f);
+    d_notice("Found n%u! Staring to solve\n", curr_n);
+    d_inc_indent();
+
+    sprintf(path, "%s/n%u/b_computational_times.out", base, curr_n);
+    time_f = fopen(path, "w");
+    if(!time_f) {
+      printf("Error opening file %s for writing...\n", path);
+      exit(1);
     }
-    average_time += test_time;
-	}
-	average_time /= files_num;
 
-	time_f = fopen(time_file, "w");
+    written_header = false;
+    tofree = string = strdup(K_list);
+    while ((token = strsep(&string, ",")) != NULL) { // For each K
+      K = strtoul(token, NULL, 10);
+      if(K == 0) {
+        continue;
+      }
+      d_notice("Solving for K = %u\n", K);
+      files_num = 0;
+      max_time = 0.0;
+      min_time = INFINITY;
+      average_time = 0.0;
 
-  fprintf(time_f, "Computational times on %zu instances:\n\n", files_num);
-  fprintf(time_f, "K = %u\n", K);
-  fprintf(time_f, "average partial time = NA\n");
-  fprintf(time_f, "average total time = %f s\n", average_time);
-  fprintf(time_f, "minimum total time = %f s\n", min_time);
-  fprintf(time_f, "maximum total time = %f s\n", max_time);
-  fprintf(time_f, "\n");
+      while(1) { // Solve each instance
+        sprintf(path, "%s/n%u/i%u/test.in", base, curr_n, files_num);
+        f = fopen(path, "r");
+        if(f == NULL) {
+          break;
+        }
+        fclose(f);
 
-	fclose(time_f);
+        solutions = single_test(path, K, &test_time, false);
 
-	sol_f = fopen(sol_file, "w");
-  print_kbest_solution_default_format(sol_f, solutions);
-	fclose(sol_f);
-  kp_free_kbest_sols(solutions);
+        sprintf(path, "%s/n%u/i%u/b_k%u_solutions.out", base, curr_n, files_num, K);
+        sol_f = fopen(path, "w");
+        if(!sol_f) {
+          printf("Error opening file %s for writing...\n", path);
+          exit(1);
+        }
+        print_kbest_solution_default_format(sol_f, solutions);
+        kp_free_kbest_sols(solutions);
+        fclose(sol_f);
+
+        if(test_time > max_time) {
+          max_time = test_time;
+        }
+        if(test_time < min_time) {
+          min_time = test_time;
+        }
+        average_time += test_time;
+        files_num++;
+      }
+      average_time /= files_num;
+
+      if(!written_header) {
+        fprintf(time_f, "Computational times on %u instances:\n\n", files_num);
+      }
+      fprintf(time_f, "K = %u\n", K);
+      fprintf(time_f, "average partial time = %f s\n", average_time);
+      fprintf(time_f, "average total time = %f s\n", average_time);
+      fprintf(time_f, "minimum total time = %f s\n", min_time);
+      fprintf(time_f, "maximum total time = %f s\n", max_time);
+      fprintf(time_f, "\n");
+
+    }
+    fclose(time_f);
+    d_notice("Done, continuing bruteforcing...\n");
+    free(tofree);
+    d_dec_indent();
+  }
 }
 
 void time_test(char* res_file, char** files, size_t files_num, uint32_t
@@ -178,26 +332,28 @@ void check_solutions(KBestSolutions solutions)
 void print_help()
 {
   d_warning(
-      "Usage: (e|t|s) [args]\n"
+      "Usage: [(e|t|s) [args]]\n"
+      "\twith no parameters the interactive mode starts\n"
       "\te <K>: example test. using ./problems/problem.txt\n"
       "\tt <k> <res_file> <num_iterations> {file}: timing test, running num_iteration iterations on the given files\n"
       "\ts <K> <file>: single test on the given file, prints the solution.\n"
-      "\tp <k> <sol_file> <time_file> {file}: total test, runs each test and prints the result in the given files\n"
+      "\tp <k_list> <base_dir> <max_n>: project test, k_list is a comma separated list of K, base_dir is dir parent of the n*** folders and max_n is and hint of the maximum *** value.\n"
       );
 }
 
 int main(int argc, char **argv)
 {
-  uint32_t num_iteration, K;
+  uint32_t num_iteration, K, max_n;
 	double total_time;
   KBestSolutions solutions;
 	//timeTest(2900, &averageTick, &averageTime, &totalTick, &totalTime);
 
-	set_debug_level(WARNING ^ ERROR);
+	set_debug_level(NOTICE ^ WARNING ^ ERROR);
 
   if(argc < 2) {
     print_help();
-    return 1;
+    interactive_test();
+    return 0;
   }
 
   switch(argv[1][0]) {
@@ -227,8 +383,8 @@ int main(int argc, char **argv)
         d_error("Mandatory parameters missing\n");
         return 6;
       }
-      K = strtoul(argv[2], NULL, 10);
-      project_test(argv[3], argv[4], &(argv[5]), argc - 5, K);
+      max_n = strtoul(argv[4], NULL, 10);
+      project_test(argv[3], argv[2], max_n);
       break;
     case 's':
       if(argc != 4) {
